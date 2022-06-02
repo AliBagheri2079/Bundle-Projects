377988d3b48f299868a5b19e45adc901380f4e53
ali bagheri <alibagheri20ab79@gmail.com>
2022-06-02 04:44:41 +0430

    add grunt project

---
+++ README.md
@@ -0,0 +1,2 @@
+## [Grunt project](./grunt-project/)
+**look grunt folder to see concating and transpiling who maked this**
---
+++ grunt-project/Gruntfile.js
@@ -0,0 +1,55 @@
+module.exports = function (grunt) {
+    grunt.initConfig({
+
+        concat: {
+            options: {
+                separator: '\n/*next file*/\n\n' //this will be put between conc. files
+            },
+            dist: {
+                src: ['assets/js/*.js'],
+                dest: 'build/js/scripts.js'
+            }
+        },
+        sass: {
+            dev: { // indicates that it will be used only during development
+                files: {
+                    // destination     // source file
+                    'build/css/main.css': 'assets/scss/bootstrap.scss'
+                }
+            }
+        },
+        cssmin: {
+            build: {
+                src: 'build/css/main.css',
+                dest: 'build/css/main.min.css'
+            }
+        },
+        watch: {
+            sass: {
+                files: '**/*.scss', // ** any directory; * any file
+                tasks: ['css']
+            },
+            concat: {
+                files: '**/*.js',
+                tasks: ['concat']
+            },
+            uglify: {
+                files: 'build/js/*.js',
+                tasks: ['uglify']
+            }
+        }
+
+    });
+
+    // Load the plugin that provides the "concat" task.
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    // Load the plugin that provides the "sass" task.
+    grunt.loadNpmTasks('grunt-contrib-sass');
+    // Load the plugin that provides the "cssmin" task.
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+
+    // Load the plugin that provides the "watch" task.
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    
+    // grunt.registerTask('js', ['concat', 'uglify']);
+};
